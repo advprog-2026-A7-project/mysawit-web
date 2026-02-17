231ea820c8be2194c70b6a357c38c2bb5729e007
azzelll <madeshandykrisnanda@gmail.com>
2026-02-18 00:15:59 +0700

    setup full CI for mysawit-web

---
+++ jest.config.mjs
@@ -0,0 +1,22 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/app/layout.tsx",
+  ],
+};
+
+export default createJestConfig(customJestConfig);
